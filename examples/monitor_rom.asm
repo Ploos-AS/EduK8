2c369a630eb5d9537ecf141148a21579c8df4a6a
pos <pgousdal@users.noreply.github.com>
2026-09-18 03:11:52 +0200

    M3: make K8 monitor ROM interactive

--- examples/monitor_rom.asm
+++ examples/monitor_rom.asm
@@ -1,16 +1,35 @@
-; Minimal K8 monitor ROM skeleton
-VIDEO_RAM = $7800
+; K8 Monitor ROM v0.1
+; Boot banner plus polling keyboard echo.
+KEY_DATA     = $C010
+KEY_STATUS   = $C011
+VIDEO_RAM    = $7800
 
 .org $8000
 reset:
     LDX #$00
-    LDA #$4B       ; 'K'
+
+    ; Visible boot banner: "K8> "
+    LDA #$4B
     STA VIDEO_RAM,X
     INX
-    LDA #$38       ; '8'
+    LDA #$38
     STA VIDEO_RAM,X
-hang:
-    JMP hang
+    INX
+    LDA #$3E
+    STA VIDEO_RAM,X
+    INX
+    LDA #$20
+    STA VIDEO_RAM,X
+    INX
+
+keyboard_wait:
+    LDA KEY_STATUS
+    AND #$01
+    BEQ keyboard_wait
+    LDA KEY_DATA
+    STA VIDEO_RAM,X
+    INX
+    JMP keyboard_wait
 
 ; K8 v1 reset and IRQ vectors
 .org $FFFC
